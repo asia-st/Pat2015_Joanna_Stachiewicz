cfa7563a52ffe296581d2be4a1bf9da43d285567
asia-st <joannastachiewicz@gmail.com>
2014-12-29 18:30:57 +0100

    Zad2

---
+++ Joanna.Stachiewicz/app/src/main/java/com/example/asia/joannastachiewicz/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.asia.joannastachiewicz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashScreen extends Activity {
+
+    private int time = 5000;
+    Thread splashThread;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        splashThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(time);
+                    intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        splashThread.start();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (splashThread.isAlive()) {
+            splashThread.interrupt();
+            splashThread = null;
+        }
+    }
+}
